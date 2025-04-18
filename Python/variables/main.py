63cfb46426830fdb2435a5a19d2601b622f11db0
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-18 08:13:27 +0600

    learnt variables in python

---
+++ Python/variables/main.py
@@ -0,0 +1,18 @@
+""" multiple command used as metadata"""
+#single command  
+
+# variables are a name given to memory location in a program
+
+name = "kakon"
+age = 25
+cgpa = 3.66
+
+print("My name is : ",name)
+print("My age: ",age)
+print("My cgpa is : ",cgpa)
+
+#variables identifier
+
+#can't start with digit
+#can be as long as i want 
+#can be a combination of uppercased lowercased digits and Underscore(_)
