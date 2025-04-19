278166d5b71e05770b5722411b716bae0eb0938b
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-19 06:04:57 +0600

    learnt data types of Python

---
+++ Python/datatypes/main.py
@@ -0,0 +1,20 @@
+#data types of python
+
+name = "kakon"
+age = 25
+cgpa = 3.66
+hasJob = True
+gotMoney = None
+
+
+print(type(name))
+print(type (age))
+print(type(cgpa))
+print(type(hasJob))
+print(type(gotMoney))
+
+#printing sum
+a=9
+b=3
+sum = a+b
+print(sum)
