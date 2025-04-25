ba1e7886ec8b0c4b377f8e0086ea31edaadb4c7d
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-25 07:51:47 +0600

    learnt basic of inputs

--- Python/typecasting,input,problmsolving/input.py
+++ Python/typecasting,input,problmsolving/input.py
@@ -1,3 +1,10 @@
 #inputs are like taking data from user
 
-input("Write your Name:")
+name = input("Write your Name:")
+print(name)
+#number = input("Enter a digit: ")
+#print(number)
+
+# here inputs always take numbers as string so we need to convert it into integer
+number = int(input("Enter a digit: "))
+print(type(number), number)
