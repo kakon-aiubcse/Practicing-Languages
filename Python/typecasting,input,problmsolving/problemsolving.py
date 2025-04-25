3e33dedf3ec3bec85beb7d4a0132e6195042fff7
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-25 07:59:33 +0600

    solved problem-1 with python

---
+++ Python/typecasting,input,problmsolving/problemsolving.py
@@ -0,0 +1,8 @@
+#take two digit from inputs and add them 
+
+firstnum = float(input("Enter first digit: "))
+secondnum = float(input("Enter second digit: "))
+
+sum = firstnum + secondnum
+
+print(sum)
