77271be65310cb4f53bf3bd93d3429d201d5e7c0
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-27 09:01:26 +0600

    wrote a program to check the occurance of specific letter in a string

---
+++ Python/strings/practice-problem2.py
@@ -0,0 +1,4 @@
+#write a program to check the occurance of $ in a string
+
+str = "$hello world. I don't like writing $php code cause I don't $know"
+print("existence of $ in ",str, "is = " , str.count("$") )
