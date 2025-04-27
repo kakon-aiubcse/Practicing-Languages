0bcca66552837463f1febaee78d0293ac225e2ce
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-27 08:53:29 +0600

    solved problem 1 to take userinput and displaying the length of the input

---
+++ Python/strings/practice-problem1.py
@@ -0,0 +1,5 @@
+#write a program to input user's first name and print it's length
+
+str = input("Enter a word to find it's length: ")
+print("your input value: ", str)
+print("length of ",str , "\t = ",len(str))
