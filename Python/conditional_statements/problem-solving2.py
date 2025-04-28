286e028fdd68ef577aea8f43cc4552be0f807ba0
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-28 10:47:31 +0600

    solved checking even or odd of a given number

---
+++ Python/conditional_statements/problem-solving2.py
@@ -0,0 +1,8 @@
+#check if a number is even or odd
+
+number = int(input("Enter number to check even or odd: "))
+
+if(number %2 == 0):
+    print("The number is even",number)
+else:
+   print("The number is odd",number)
