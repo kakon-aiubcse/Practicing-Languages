a75cc907686847940517920578fc70da3296cdca
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-28 10:36:23 +0600

    learnt if else conditional statements on python

---
+++ Python/conditional_statements/cs.py
@@ -0,0 +1,12 @@
+#if-elif-else
+
+age = 27
+
+if(age>=18 and age<=25):
+    print("can vote and apply for licence")
+    print(age)
+elif(age<=17):
+    print("can't vote")
+    print(age)
+else: 
+    print("over-aged")
