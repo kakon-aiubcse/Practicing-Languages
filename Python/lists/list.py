0b298468385d85270fb3e6f6e90bbb70a3fbb2ae
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-29 18:34:35 +0600

    learnt list in python

---
+++ Python/lists/list.py
@@ -0,0 +1,15 @@
+#learning list similar to array
+
+marks = [34,33,22.3,45,25]
+print(marks)
+print(len(marks))
+print(type(marks))
+print(marks[3])
+
+#can be any type in list 
+#strings are immutable but lists are mutuable
+
+student = ['kakon', 25, 3.69, 'unemployed']
+print(student)
+student[1] = 26
+print(student)
