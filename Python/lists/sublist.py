c2424fe3634b87dab906ebbc338b02caf6d89d1e
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-29 18:42:09 +0600

    learnt sublist in javascript

---
+++ Python/lists/sublist.py
@@ -0,0 +1,7 @@
+#sublist is like slicing the list items
+
+marks =[85,94, 59, 93,23]
+
+print(marks[1:]) # this will print values after 1
+print(marks[-3:-1]) # this will print 59 93 but not the minus one like before minus one
+print(marks[:5])
