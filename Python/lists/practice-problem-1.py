193702319919b846937ffd9efc292a4554b32c7c
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-30 23:52:51 +0600

    wrote a funtion to take 3 car's name and save it in an array

---
+++ Python/lists/practice-problem-1.py
@@ -0,0 +1,7 @@
+#write a function take 3 car's name and save it in a list 
+
+cars = []
+cars.append(input("Enter the name of first cars: "))
+cars.append(input("Enter the name of second cars: "))
+cars.append(input("Enter the name of third cars: "))
+print("Name of Cars: ",cars)
