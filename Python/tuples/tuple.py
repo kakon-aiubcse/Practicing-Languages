ccee22c9981ac4d8619de9619f91277b501df1b4
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-30 23:45:03 +0600

    learnt tuples in python

---
+++ Python/tuples/tuple.py
@@ -0,0 +1,11 @@
+#tuples in array
+#tuples are like string immutable means can't change values 
+#for making tuples we use paranthesis
+tup = (2,2,4,'abcd',3)
+print(tup)
+print(type(tup))
+#should give first value with comma to define tuple 
+print(tup[1:3])
+print(tup.count('abcd'))
+print(tup.index('abcd'))
+print(len(tup))
