4e70abed8555711ddc62ccfe1e82d091666747f2
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-05-20 05:30:47 +0600

    learnt set methods

---
+++ Python/set/setsmethod.py
@@ -0,0 +1,15 @@
+collection = set()
+collection.add("kakon")
+collection.add(1)
+
+collection.add("Khairul")
+collection.add(2)
+collection.add(3)
+print(collection)
+print(collection.pop())
+print(collection)
+
+seta = {1,2,3}
+setb = {2,3,4}
+print(seta.union(setb))
+print(seta.intersection(setb))
