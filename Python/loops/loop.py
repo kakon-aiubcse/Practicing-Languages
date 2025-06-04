c5438294f39223a49569a103875528da444d7e13
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-05 02:49:51 +0600

    learnt while loops

---
+++ Python/loops/loop.py
@@ -0,0 +1,6 @@
+#loop firstly setting up the condintion then statements then updation
+
+i = 1 #here initialised iteration
+while i <= 5: #here while keyword with condition of i is less then or equal 5 (if true it will print the statement)
+    print(i)
+    i+= 1 # here i += 1 means i will be increased with 1
