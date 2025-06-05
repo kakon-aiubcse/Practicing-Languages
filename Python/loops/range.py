04862ba92045b8e8cb8376eacde78775e2aa5ba4
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-05 07:57:06 +0600

    learnt range in for loop

---
+++ Python/loops/range.py
@@ -0,0 +1,25 @@
+#range in python keeps three things
+#start which is optional
+#stop which is must
+#step which optional by default 1
+
+for x in range(10):
+    print(x) #here 10 is must stop param starts from 0 and ends in 10-1 = 9
+print(
+    "________"
+)
+for x in range(2,5):
+    print(x) # here range start from 2 and stops in 4
+    
+print(
+    "________"
+)
+
+for x in range(3,9,2):
+    print(x) #here start 3 stops 9 and steps 2
+   
+print(
+    "________"
+)
+
+
