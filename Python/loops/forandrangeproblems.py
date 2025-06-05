c053ee746e76f68045d0939ec0e91eb9cf9c8e3a
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-05 08:03:56 +0600

    problem solving on for range loop

---
+++ Python/loops/forandrangeproblems.py
@@ -0,0 +1,21 @@
+#print numbers from 1 to 100
+for x in range(1,101):
+    print(x)
+
+print(
+    "________"
+)
+#print numbers from 100 to 1
+for x in range(100,0, -1):
+    print(x)
+
+print(
+    "________"
+)
+#print the multiplication of number n
+n = 3 
+for x in range(1,11):
+    print(n*x)
+print(
+    "________"
+)
