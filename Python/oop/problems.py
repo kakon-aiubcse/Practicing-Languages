19d4d454c534fa7cda9e41007a238e0ff9f7b9f3
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-20 02:55:12 +0600

    solved problem related to class in python

---
+++ Python/oop/problems.py
@@ -0,0 +1,19 @@
+#problem 1
+#create a student class that takes name and marks of 3 subjects as arguments of constructor and create a method to print average
+
+class Student: 
+    def __init__(self, name1, name2, name3, marks1, marks2,marks3):
+        self.name1 = name1
+        self.name3 = name3
+        self.name2 = name2
+        self.marks1 = marks1
+        self.marks2 = marks2
+        self.marks3 = marks3
+        print(self.name1, self.name2, self.name3, self.marks1, self.marks2, self.marks3)
+    def average(self):
+            sum = self.marks3+self.marks1+self.marks2
+            avg = sum/3
+            return avg
+
+s1 = Student("physics", "chemisty", "math", 88,97,66)
+print(s1.average())
