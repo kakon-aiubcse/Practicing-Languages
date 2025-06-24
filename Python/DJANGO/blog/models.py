ceb6b666bd555fa898372e2d2c29502b44b1d004
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-24 22:00:22 +0600

    managed to access admin database of django and created object

--- Python/DJANGO/blog/models.py
+++ Python/DJANGO/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    titles = models.CharField(max_length=40)
+    content = models.TextField()
